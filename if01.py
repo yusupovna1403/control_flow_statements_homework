01369dd9a68858f6e9ab4f13bcc21dcf6ccdecf3
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-28 18:18:16 +0500

    if-else_01

--- if01.py
+++ if01.py
@@ -6,4 +6,9 @@ def main(a):
     Returns:
         a: a increased by 1 if positive, else unchanged.
     """
-    return
+    if a > 0:
+        return a + 1
+    else:
+        return a
+print(main(1))
+print(main(-5))
