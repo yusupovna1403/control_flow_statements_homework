0aa16f9b204e93529d66eaeb4687d379bd41c27f
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-28 19:43:49 +0500

    if-else_08

--- if08.py
+++ if08.py
@@ -11,4 +11,16 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a > 9 and a < 100 or a > -100 and a < -9:
+        if a % 2 == 0:
+            return "two-digit even number"
+        else:
+            return "two-digit odd number"
+    if a > 99 and a < 1000 or a > - 1000 and a < -99:
+        if a % 2 == 0:
+            return "three-digit even number"
+        else:
+            return "three-digit odd number"
+
+print(main(-242))
+print(main(57))
