bda3fc5b34c7650af8e9253befcddfece9478aab
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-28 18:34:14 +0500

    if-else_03

--- if03.py
+++ if03.py
@@ -6,4 +6,12 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    if a > 0:
+        return a + 1
+    elif a < 0:
+        return a - 2
+    else:
+        return 10
+print(main(-9))
+print(main(3))
+print(main(0))
